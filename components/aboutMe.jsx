77046fd390a0b70fe9a4b0280aabfec31206b901
Jay Gurjar <jaymgurjar@gmail.com>
2022-11-09 14:16:11 -0700

    Font changes and image revamp

--- components/aboutMe.jsx
+++ components/aboutMe.jsx
@@ -33,21 +33,22 @@ const AboutMe = (props) => {
                         width={100}
                         style={{display: 'revert'}}
                     />
-                    <h3 className=' text-lg font-mono font-medium pt-4 pb-2'>
+                    <h3 className=' text-lg font-burtons font-medium pt-4 pb-2'>
                         Full Stack Development
                     </h3>
-                    <p className=' font-mono'>
+                    <p className=' font-nimbusRegular'>
                         2+ years of experience in developing full stack web applications with an immersive
                         front end and scalable backend.
                     </p>
                     <h4 className=' py-4 font-semibold text-teal-600'>
                         Tools I use
                     </h4>
-                    <li className=' font-mono text-grey-800 py-1'>JQuery to manipulate DOM</li>
-                    <li className=' font-mono text-grey-800 py-1'>Jest to test UI components</li>
-                    <li className=' font-mono text-grey-800 py-1'>React on top of JS/TS</li>
-                    <li className=' font-mono text-grey-800 py-1'>Puppeteer to end-to-end testing</li>
-                    <li className=' font-mono text-grey-800 py-1 pb-4'>Experienced in using Spring Boot, Amplify or Flask as backend frameworks</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1'><span className="text-teal-500"> JQuery </span> to manipulate DOM</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1'><span className="text-teal-500"> Jest </span> to test UI components</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1'> <span className="text-teal-500">React </span> on top of JS/TS</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1'><span className="text-teal-500"> Puppeteer </span> to end-to-end testing</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1'>Used<span className="text-teal-500"> NextJS </span> for SSR applications</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1 pb-4'>Experienced in using <span className="text-teal-500"> Spring Boot, Amplify or Flask </span> as backend frameworks</li>
                 </div>
                 <div className='text-center shadow-2xl p-10 rounded-2xl my-10 lg:w-1/3'>
                     <Image
@@ -56,18 +57,18 @@ const AboutMe = (props) => {
                         height={100}
                         style={{display: 'revert'}}
                     />
-                    <h3 className=' text-lg font-mono font-medium pt-4 pb-2'>
+                    <h3 className=' text-lg font-burtons font-medium pt-4 pb-2'>
                         OOP Scripting
                     </h3>
-                    <p className=' font-mono'>
+                    <p className=' font-nimbusRegular'>
                         3 years of academic scripting experience
                     </p>
                     <h4 className=' py-4 font-semibold text-teal-600'>
                         Tools I use
                     </h4>
-                    <li className=' font-mono text-grey-800 py-1'>Experinced in scripting using C, C++ and Java </li>
-                    <li className=' font-mono text-grey-800 py-1'>Used C++ to program micro-processors for a project</li>
-                    <li className=' font-mono text-grey-800 py-1 pb-4'>Used Python to program a stock-market statement generator</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1'>Experinced in scripting using <span className="text-teal-500"> C, C++ and Python </span> </li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1'>Used C++ to program <span className="text-teal-500"> micro-processors </span> for a project</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1 pb-4'>Used <span className="text-teal-500"> Python </span> to program a stock-market statement generator</li>
                 </div>
                 <div className='text-center shadow-2xl p-10 rounded-2xl my-10 lg:w-1/3'>
                     <Image
@@ -75,19 +76,19 @@ const AboutMe = (props) => {
                         width={50}
                         style={{display: 'revert'}}
                     />
-                    <h3 className=' text-lg font-mono font-medium pt-4 pb-2'>
+                    <h3 className=' text-lg font-burtons font-medium pt-4 pb-2'>
                         Andriod/IOS Development
                     </h3>
-                    <p className='font-mono'>
+                    <p className='font-nimbusRegular'>
                         Almost 1 year of experience in developing full stack mobile applications with an immersive
                         UI and scalable backend.
                     </p>
                     <h4 className=' py-4 font-semibold text-teal-600'>
                         Tools I use
                     </h4>
-                    <li className=' font-mono text-grey-800 py-1'>React Native on top of JS/TS</li>
-                    <li className=' font-mono text-grey-800 py-1'>Kotlin/Swift for some OS specific features</li>
-                    <li className=' font-mono text-grey-800 py-1 pb-4'>Experienced in using Amplify backend frameworks</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1'><span className="text-teal-500">React Native</span> on top of JS/TS</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1'><span className="text-teal-500">Kotlin/Swift</span> for some OS specific features</li>
+                    <li className=' font-nimbusRegular text-grey-800 py-1 pb-4'>Experienced in using <span className="text-teal-500">Amplify </span> backend frameworks</li>
                 </div>
                 
             </div>
